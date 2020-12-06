1679ded44c2f4c0141be202c3371286cca78c4e9
Kasper854 <72204794+Kasper854@users.noreply.github.com>
2020-12-06 13:44:48 +0200

    Add files via upload

---
+++ stopwatch/sekundomer/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace sekundomer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(53, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "On";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(28, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 71);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(50, 50);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "T";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ stopwatch/sekundomer/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace sekundomer
+{
+    public partial class Form1 : Form
+    {
+        DateTime date1 = new DateTime(0, 0);
+        Form topMostForm;
+        Label textTimer;
+        Button resetTimer;
+        Button saveTime;
+        static int index = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            
+           
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            date1 = date1.AddSeconds(1);
+            textTimer.Text = date1.ToString("HH:mm:ss");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+                topMostForm = new Form();
+                topMostForm.Size = new Size(15,90);//15 90
+                topMostForm.StartPosition = FormStartPosition.Manual;
+                topMostForm.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+                textTimer = new Label();
+                textTimer.Font = new Font("Arial",15, FontStyle.Bold);
+                textTimer.Location = new Point(17, 0);
+                saveTime = new Button();
+                saveTime.Size = new Size(45, 25);
+                saveTime.Location = new Point(10, 23);
+                saveTime.Text = "Save";
+                saveTime.FlatAppearance.BorderSize = 0;
+                saveTime.FlatStyle = FlatStyle.Flat;
+                saveTime.BackColor = Color.Aquamarine;
+                resetTimer = new Button();
+                resetTimer.Size = new Size(45, 25);
+                resetTimer.Location = new Point(65, 23);
+                resetTimer.Text = "Reset";
+                resetTimer.FlatAppearance.BorderSize = 0;
+                resetTimer.FlatStyle = FlatStyle.Flat;
+                resetTimer.BackColor = Color.Red;
+
+
+
+
+            saveTime.Click += SaveTime_Click;
+                resetTimer.Click += ResetTimer_Click;
+
+                topMostForm.Controls.Add(textTimer);
+              
+                topMostForm.Controls.Add(resetTimer);
+                topMostForm.Controls.Add(saveTime);
+                topMostForm.TopMost = true;
+               
+                topMostForm.Show();
+               
+
+            if(timer1.Enabled==true)
+            {
+                timer1.Stop();
+                timer1.Enabled = false;    
+            }
+            else
+            {
+                timer1.Interval = 1000;
+                button1.Enabled = false;
+                timer1.Enabled = true;
+                timer1.Start();
+            
+            }
+            topMostForm.FormClosed += TopMostForm_FormClosed;
+        }
+
+        private void ResetTimer_Click(object sender, EventArgs e)
+        {
+            date1 = new DateTime(0, 0);
+            textTimer.Text = "00:00:00";
+            
+        }
+
+        private void SaveTime_Click(object sender, EventArgs e)
+        {
+            index++;
+            comboBox1.Items.Add(index + " | " + textTimer.Text);
+        }
+
+        private void TopMostForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            textTimer.Text = "0";
+            timer1.Enabled = false;
+            button1.Enabled = true;
+            date1 = new DateTime(0, 0);
+        }
+
+  
+    }
+}
